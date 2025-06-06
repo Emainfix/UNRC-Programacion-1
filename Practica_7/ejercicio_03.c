32f691e1b6771ec9ab60d4c3a8608b1dda42efd2
Emainfix <fev.artist@gmail.com>
2025-06-06 18:01:59 -0300

    Inicio de las actividades sobre arrays

---
+++ Practica_7/ejercicio_03.c
@@ -0,0 +1,6 @@
+/*En un Bingo se registran los números que van saliendo hasta que haya un ganador, o 
+eventualmente se saquen 30 números. Luego se debe informar los números que salieron. Diseñar un 
+algoritmo que permita ir almacenando los números que salen en una estructura de datos adecuada y si 
+hay un ganador que se detenga la carga de números. Posteriormente todos los números que fueron 
+almacenados deben ser mostrados, la condición de ganador se evalúa mediante la pregunta ¿Hay 
+ganador?, N significa que no hay ganador, S significa que  hay ganador. */
