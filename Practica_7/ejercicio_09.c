a7457fc158902aa14ff5dd90a925166234fe2d44
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-06-12 18:52:27 -0300

    Determina si una palabra es capicua

---
+++ Practica_7/ejercicio_09.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#define NMax 20
+
+typedef struct
+{
+    char a[NMax];
+    int cant;
+}TData;
+
+TData palara;
+
+void CargarPalabra(TData *p);
+int Capicua (TData p);
+
+int main()
+{
+    CargarPalabra(&palara);
+    
+    if (Capicua (palara))
+    {
+        printf("VERDADERO: Es capicua.");
+    }else{
+        printf("FALSO: No es capicua.");
+    }
+}
+
+void CargarPalabra(TData *p)
+{
+    int i;
+    char letra;
+    printf("Ingrese la cantidad de caracteres del arreglo: ");
+    scanf("%d",&p->cant);
+    while (p->cant > 20 || p->cant < 1)
+    {
+        printf("La cantidad debe estar entre 1 y 20: ");
+        scanf("%d",&p->cant);
+    }
+    printf("Ingrese la palara una letra a la vez: \n");
+    for (i = 0; i < p->cant; i++)
+    {
+        printf("Ingrese la letra %d: ",i);
+        scanf(" %c",&letra);
+        p->a[i] = letra;
+    }
+    
+}
+
+int Capicua (TData p)
+{
+    int k,i, control;
+    TData q;
+
+    i = 0;
+    k = p.cant - 1;
+    while (k >= 0)
+    {
+        q.a[i] = p.a[k];
+        i++;
+        k--;
+    }
+
+    for (i = 1; i < p.cant; i ++)
+    {
+        if (p.a[i] != q.a[i])
+        {
+            control = 0;
+        }else{
+            control = 1;
+        }
+    }
+
+    return control;
+}
